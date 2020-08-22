f6febc8aa0212db0a7b4ca5b5caad00d6d8e4ee1
Justice Payne <spikepayne@yahoo.com>
2020-08-22 21:36:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
   if phrase == "i love you grandma!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.downcase
-    return  "HUH?! SPEAK UP SONNY!"
+    return  "I LOVE YOU TOO PUMPKIN!"
   else phrase == "i love you GRandma"
     return "HUH?! SPEAK UP SONNY!"
   end
